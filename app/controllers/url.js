440cfabdbdddb0acd28e2cc8ca89a1a78765015a
Sean Kennedy <kennedymsean@gmail.com>
2015-12-16 02:37:05 -0500

    Null check on url document from mongo

--- app/controllers/url.js
+++ app/controllers/url.js
@@ -1,27 +1,31 @@
-// Core modules
-var http = require('http');
-
 // App modules
-var express = require('express');
 var mongoose = require('mongoose');
 var URL = mongoose.model('URL');
+
+// Helper modules
+var tools = require('../tools/tools');
 
 module.exports = function(app, config) {
     app.get('/:token([a-zA-Z0-9-_]{5})', function(request, response, next) {
             if (request.params.token) {
                 URL.findOne({ token: request.params.token }, function(error, url) {
-                    if (error) {
-                        console.log(error);
-                    }
-                    if (url.paid) {
-                        response.writeHead(301, {Location: url.url});
-                        response.end();
-                    } else {
-                        response.render('url', url);
-                    }
-                });
-            } else {
-                next();
+						if (error) {
+							console.log(error);
+						} else if (url != null) {
+							if (tools.isDefined(url.url)) {
+								if (tools.isDefined(url.paid) && url.paid) {
+									response.writeHead(301, {Location: url.url});
+									response.end();
+								} else {
+									response.render('url', url);
+								}
+							}
+						}
+					}
+				);
             }
-        });
+			
+			next();
+        }
+	);
 };
